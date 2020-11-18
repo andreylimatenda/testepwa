27a6445fbc26899df1c550a325e2b22c9af11bf0
andreylimatenda <72941360+andreylimatenda@users.noreply.github.com>
2020-11-18 16:14:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 if('serviceWorker' in navigator) {
   navigator.serviceWorker
-           .register('sw.js')
+           .register('testepwa/sw.js')
            .then(function() { console.log('Service Worker Registered'); });
 }
   
